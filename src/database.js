a20302e0c7ac48b6171f8f9e3a54ab5fcc02bd21
BritanyChuma1717 <britanykarolina@gmail.com>
2026-02-26 12:49:28 -0500

    bd

--- src/database.js
+++ src/database.js
@@ -5,20 +5,18 @@ dotenv.config()
 
 mongoose.set('strictQuery', true)
 
-const connection = async () => {
+const connection = async()=>{
+    const mongoUri =
+        process.env.MONGODB_URI_PRODUCTION ||
+        process.env.MONGODB_URI_LOCAL ||
+        process.env.MONGODB_URI
+
+    if (!mongoUri) {
+        throw new Error('No existe MONGODB_URI_PRODUCTION, MONGODB_URI_LOCAL o MONGODB_URI en .env')
+    }
+
     try {
-
-        const uri =
-            process.env.MONGODB_URI_PRODUCTION 
-
-        if (!uri) {
-            throw new Error('No hay URI de MongoDB definida en .env')
-        }
-
-        console.log('Conectando a:', uri)
-
-        const { connection } = await mongoose.connect(uri)
-
+        const {connection} = await mongoose.connect(mongoUri)
         console.log(`Database connected on ${connection.host} - ${connection.port}`)
 
     } catch (error) {
